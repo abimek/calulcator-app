422146e084c4783dc2e3807d117b1649567cc97b
abimekuriya <abi.mek2005@gmail.com>
2021-10-12 22:09:22 -0400

    Implemented the icon and only allowing a single instance of the app to run

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 const {app, BrowserWindow} = require('electron')
 
+const win = null;
 
-app.on('ready', () => {
-    const win = new BrowserWindow({
-        height: 608,
-        width: 506,
-        show: false,
-        resizable: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    win.loadFile('index.html')
+const gotTheLock = app.requestSingleInstanceLock()
 
-    win.webContents.on('did-finish-load', () => {
-        win.show();
+
+if (!gotTheLock) {
+    app.quit()
+  } else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      // Someone tried to run a second instance, we should focus our window.
+      if (win) {
+        if (win.isMinimized()) win.restore()
+        win.focus()
+      }
     })
+      
+    // Create myWindow, load the rest of the app, etc...
+    app.on('ready', () => {
+        const win = new BrowserWindow({
+            icon: "CalcAppIcon.png",
+            height: 608,
+            width: 506,
+            show: false,
+            resizable: false,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
+        win.loadFile('index.html')
+    
+        win.webContents.on('did-finish-load', () => {
+            win.show();
+        })
+    })
+  }
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
 })
-
-app.on('window-all-closed', () => app.quit)
 
